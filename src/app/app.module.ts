d622c4b642569d0a2b8900872d2b4853e4eba1c1
Stepan Suvorov <stevermeister@gmail.com>
2021-09-11 13:54:21 +0200

    add virtual scroll example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
